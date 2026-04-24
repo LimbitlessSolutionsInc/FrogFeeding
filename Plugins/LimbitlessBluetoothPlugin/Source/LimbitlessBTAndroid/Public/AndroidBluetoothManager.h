2084a637081efc8678f4b66da08a5e3a78f5fd1e
Sameer Bashir <s.bashir@limbitless-solutions.org>
2026-04-24 10:28:40 -0400

    plugin update

--- Plugins/LimbitlessBluetoothPlugin/Source/LimbitlessBTAndroid/Public/AndroidBluetoothManager.h
+++ Plugins/LimbitlessBluetoothPlugin/Source/LimbitlessBTAndroid/Public/AndroidBluetoothManager.h
@@ -2,41 +2,45 @@
 
 #pragma once
 
-#include <winrt/base.h>
-
 #include "CoreMinimal.h"
+#include "Containers/Queue.h"
 #include "Managers/BluetoothManager.h"
 #include "UObject/Object.h"
-#include "WindowsBluetoothManager.generated.h"
+#include "AndroidBluetoothManager.generated.h"
 
-class UWinBLEManager;
+class IBLEManagerInterface;
+class UFlexController;
+class IBLEDeviceInterface;
 /**
  * 
  */
 UCLASS()
-class LIMBITLESSBTWINDOWS_API UWindowsBluetoothManager : public UObject, public IBluetoothManager
+class LIMBITLESSBTANDROID_API UAndroidBluetoothManager : public UObject, public IBluetoothManager, public FTickableGameObject
 {
 	GENERATED_BODY()
 public:
-	void CreateWinBLEManager();
 	virtual void Init(const FString& writeCharacteristic) override;
 	virtual void Deinit() override;
-	
-	winrt::guid FStringToWinrtGUID(const FString& fstr) const;
-	void ConvertFStringArrayToGuidArray(const TArray<FString>& fstrings, TArray<winrt::guid>& guids) const;
 
 	virtual void SetServiceUUIDsToScanFor(const TArray<FString>& filteredServiceUUIDs) override;
 	virtual void ScanForDevices(const FOnFlexFoundSignature& onDeviceFoundCallback) override;
 	virtual void StopScan() override;
 	virtual void ConnectToDevice(UFlexController* device, const TArray<FString> characteristicIds, const FOnFlexConnectedSignature& onDeviceConnectedCallback, const FOnFlexConnectionErrorSignature& onFailToConnectDeviceCallback, const FOnFlexServiceSetSignature& onServiceSetCallback) const override;
 	virtual void DisconnectDevice(const UFlexController* device, const FOnFlexDisconnectedSignature& onDeviceDisconnectedCallback, const FOnFlexDisconnectionErrorSignature& onFailToDisconnectDeviceCallback) const override;
-	
-private:
-	UPROPERTY()
-	TObjectPtr<UWinBLEManager> WinBLEManager;
 
-	TMap<uint64_t, UFlexController*> CreatedControllers{};
+	virtual bool IsTickable() const override;
+	virtual bool IsTickableInEditor() const override;
+	virtual bool IsTickableWhenPaused() const override;
+	virtual bool IsAllowedToTick() const override;
+	virtual void Tick(float DeltaTime) override;
+	virtual TStatId GetStatId() const override { return Super::GetStatID(); }
+private:
+	TScriptInterface<IBLEManagerInterface> BLEManager;
+	TMap<FString, UFlexController*> CreatedControllers{};
+	FString WriteCharacteristic;
+
 	
-	winrt::guid WriteCharacteristicUUID;
-	
+	FOnFlexFoundSignature OnDeviceFoundCallback;
+	TQueue<TScriptInterface<IBLEDeviceInterface>, EQueueMode::Mpsc> FoundDeviceQueue;
+
 };
